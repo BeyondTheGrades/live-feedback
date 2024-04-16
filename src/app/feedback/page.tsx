76119d03acca15f71b46d0771362ced07d325a5a
Prathap Chandra <prathap.chandra.754@gmail.com>
2024-04-16 22:12:03 +0530

    first commit

---
+++ src/app/feedback/page.tsx
@@ -0,0 +1,7 @@
+export default function feedbackPage() {
+  return (
+    <div>
+      <h1>Feedback</h1>
+    </div>
+  );
+}
---
+++ src/app/workshops/page.tsx
@@ -0,0 +1,7 @@
+export default function WorkshopsPage() {
+  return (
+    <div>
+      <h1>Workshops</h1>
+    </div>
+  );
+}
